096d87255940de04946c6a7e8e6f04b3642feec8
diego-nascimento <diegostealth3@gmail.com>
2021-07-04 01:38:44 -0300

    refactor: linting

--- src/Data/UseCases/Get-Data.ts
+++ src/Data/UseCases/Get-Data.ts
@@ -1,15 +1,15 @@
-import { HttpRequest, HttpResponse } from '../../Domain/Protocols/httpHelpers';
-import {IGet} from '../../Domain/UseCases/Get'
-import { IGetInfra } from '../Protocols/IGetInfra ';
+import { HttpRequest, HttpResponse } from '../../Domain/Protocols/httpHelpers'
+import { IGet } from '../../Domain/UseCases/Get'
+import { IGetInfra } from '../Protocols/IGetInfra '
 
-
-export class GetData implements IGet{
+export class GetData implements IGet {
   private readonly GetInfra: IGetInfra
 
-  constructor(GetInfra: IGetInfra){
+  constructor (GetInfra: IGetInfra) {
     this.GetInfra = GetInfra
   }
-  async handle(httpRequest: HttpRequest):Promise<HttpResponse>{
+
+  async handle (httpRequest: HttpRequest):Promise<HttpResponse> {
     const response = await this.GetInfra.Get(httpRequest)
     switch (response.statusCode) {
       case 200:
@@ -21,7 +21,7 @@ export class GetData implements IGet{
         return {
           statusCode: response.statusCode,
           body: 'Algo deu errado, tente Novamente Mais tarde'
-        } 
+        }
     }
   }
 }
