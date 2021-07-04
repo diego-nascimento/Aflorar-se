7c254d23bdde1db15dc9c3893714b64f77b59178
diego-nascimento <diegostealth3@gmail.com>
2021-07-04 01:38:18 -0300

    feat: test lint-staged

--- src/Infra/axios/GetInfra.ts
+++ src/Infra/axios/GetInfra.ts
@@ -1,18 +1,17 @@
 
-import { IGetInfra, IGetEntry } from '../../Data/Protocols/IGetInfra ';
+import { IGetInfra, IGetEntry } from '../../Data/Protocols/IGetInfra '
 import axios from 'axios'
-import { HttpResponse } from '../../Domain/Protocols/httpHelpers';
+import { HttpResponse } from '../../Domain/Protocols/httpHelpers'
 
-
-export class GetInfra implements IGetInfra{
-  async Get(httpRequest: IGetEntry):Promise<HttpResponse>{
+export class GetInfra implements IGetInfra {
+  async Get (httpRequest: IGetEntry):Promise<HttpResponse> {
     const url = httpRequest.url
     try {
       const response = await axios.get(url, httpRequest.body)
       const data:HttpResponse = {
         statusCode: response.status,
         body: response.data
-      } 
+      }
       return data
     } catch (error) {
       return {
